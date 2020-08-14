90fc8455c9d7d7c96286022b5992001793548754
Will Hack <will.j.hack@gmail.com>
2020-08-14 19:07:17 -0400

    Initial build!

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './build',
+    hot: true,
+    publicPath: '/',
+    proxy: {
+      '/server': 'http://localhost:3000',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { node: 'current' } }],
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s?css/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.css', '.scss', '.js', '.jsx'],
+  },
+};
